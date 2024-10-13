c9ea745bb22a71cff7d9d40ff094a18a11433f6c
Pratham <prathammaharjan1939@gmail.com>
2024-10-14 00:09:21 +1030

    upload files

--- question08.sh
+++ question08.sh
@@ -1,4 +1,4 @@
 mkdir dir2
 mv *.txt dir2
-git add dir2
+git add dir2/*.txt
 git commit -m "Move text files to dir2"
--- question11.sh
+++ question11.sh
@@ -1,7 +1,7 @@
-git branch branch2
-git checkout branch2
+git checkout -b branch2
 touch file4
 git add file4
 git commit -m "Add file4"
+nano file4
 git stash
 git checkout main
