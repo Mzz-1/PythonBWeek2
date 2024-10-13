5a9b43b5de6e97dc4b08633b160793d5127a8249
Pratham <prathammaharjan1939@gmail.com>
2024-10-14 00:04:34 +1030

    upload files

---
+++ question13.sh
@@ -0,0 +1,8 @@
+
+git branch  Q13
+git checkout Q13
+rm *.sh
+touch file13.txt
+git add file13.txt
+git commit -m "Add file13.txt and remove .sh files"
+git push origin Q13
---
+++ question15.sh
@@ -0,0 +1,6 @@
+git checkout main
+git fetch origin branch1
+git merge branch1
+
+git add .
+git commit -m "Merge branch1 with main and resolve conflicts"
